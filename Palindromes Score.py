3b5a3f169f58b21b5bd9d1c3eeecec667bb407a5
Alex Ford <alex@ford-web.net>
2018-09-12 18:02:51 -0500

    Better structure and more complete output.

--- Palindromes Score.py
+++ Palindromes Score.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python
 import sys
+import argparse
 
 def almost_palindrome(str):
 	str = str.upper()
+	if len(str) == 0:
+		raise ValueError("Palindrome score cannot be calculated on empy strings.")
 	score = 0
 	for i in range(0, len(str)):
 		if str[i] != str[-(i+1)]:
 			score += 1
-	return score
+	return {
+		"score": score,
+		"length": len(str),
+		"ratio": (len(str)-score)/len(str)
+	}
+
+def main():
+	word = sys.argv[1]
+	print("{word:s}: {score[score]:3d}/{score[length]:3d}  ({score[ratio]:.1%})".format(word=word, score=almost_palindrome(word)))
 
 if __name__ == '__main__':
-	print("{word:s}: {score:3d}".format(word=sys.argv[1], score=almost_palindrome(sys.argv[1])))
+	main()
